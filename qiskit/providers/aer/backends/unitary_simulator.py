717f5b0f0939ade5ed2f5f7bba3d228af3076ab5
Christopher Wood <cjwood@us.ibm.com>
2020-07-08 17:08:05 -0400

    update circuit simulators

--- qiskit/providers/aer/backends/unitary_simulator.py
+++ qiskit/providers/aer/backends/unitary_simulator.py
@@ -11,7 +11,6 @@
 # that they have been altered from the originals.
 
 # pylint: disable=invalid-name
-
 """
 Qiskit Aer Unitary Simulator Backend.
 """
@@ -23,7 +22,8 @@ from qiskit.providers.models import QasmBackendConfiguration
 from qiskit.providers.aer.backends.aerbackend import AerBackend
 from qiskit.providers.aer.backends.backend_utils import (backend_gates,
                                                          available_methods,
-                                                         MAX_QUBITS_STATEVECTOR)
+                                                         MAX_QUBITS_STATEVECTOR
+                                                         )
 from qiskit.providers.aer.aererror import AerError
 from qiskit.providers.aer.version import __version__
 # pylint: disable=import-error, no-name-in-module
@@ -32,19 +32,36 @@ from qiskit.providers.aer.backends.controller_wrappers import unitary_controller
 # Logger
 logger = logging.getLogger(__name__)
 
-AVAILABLE_METHODS = available_methods(
-    unitary_controller_execute, [
-        'automatic',
-        'unitary',
-        'unitary_gpu',
-        'unitary_thrust'
-    ])
-
 BASIS_GATES = [
-    'u1', 'u2', 'u3', 'cx', 'cz', 'id', 'x', 'y', 'z', 'h', 's', 'sdg',
-    't', 'tdg', 'swap', 'ccx', 'unitary', 'cu1', 'cu2',
-    'cu3', 'cswap', 'mcx', 'mcy', 'mcz', 'mcu1', 'mcu2', 'mcu3',
-    'mcswap', 'multiplexer',
+    'u1',
+    'u2',
+    'u3',
+    'cx',
+    'cz',
+    'id',
+    'x',
+    'y',
+    'z',
+    'h',
+    's',
+    'sdg',
+    't',
+    'tdg',
+    'swap',
+    'ccx',
+    'unitary',
+    'cu1',
+    'cu2',
+    'cu3',
+    'cswap',
+    'mcx',
+    'mcy',
+    'mcz',
+    'mcu1',
+    'mcu2',
+    'mcu3',
+    'mcswap',
+    'multiplexer',
 ]
 
 DEFAULT_CONFIGURATION = {
@@ -58,10 +75,10 @@ DEFAULT_CONFIGURATION = {
     'open_pulse': False,
     'memory': False,
     'max_shots': int(1e6),  # Note that this backend will only ever
-                            # perform a single shot. This value is just
-                            # so that the default shot value for execute
-                            # will not raise an error when trying to run
-                            # a simulation
+    # perform a single shot. This value is just
+    # so that the default shot value for execute
+    # will not raise an error when trying to run
+    # a simulation
     'description': 'A C++ unitary simulator for QASM Qobj files',
     'coupling_map': None,
     'basis_gates': BASIS_GATES,
@@ -113,14 +130,39 @@ class UnitarySimulator(AerBackend):
       performance (Default: 14).
     """
 
-    def __init__(self,
-                 provider=None,
-                 **backend_options):
-        super().__init__(QasmBackendConfiguration.from_dict(DEFAULT_CONFIGURATION),
-                         available_methods=AVAILABLE_METHODS,
-                         provider=provider,
-                         controller=unitary_controller_execute,
-                         backend_options=backend_options)
+    _AVAILABLE_METHODS = None
+
+    def __init__(self, provider=None, **backend_options):
+
+        if UnitarySimulator._AVAILABLE_METHODS is None:
+            UnitarySimulator._AVAILABLE_METHODS = available_methods(
+                unitary_controller_execute,
+                ['automatic', 'unitary', 'unitary_gpu', 'unitary_thrust'])
+
+        super().__init__(
+            QasmBackendConfiguration.from_dict(DEFAULT_CONFIGURATION),
+            available_methods=UnitarySimulator._AVAILABLE_METHODS,
+            provider=provider,
+            backend_options=backend_options)
+
+    def _execute(self, qobj, run_config):
+        """Execute a qobj on the backend.
+
+        Args:
+            qobj (QasmQobj): simulator input.
+            run_config (dict): run config for overriding Qobj config.
+
+        Returns:
+            dict: return a dictionary of results.
+        """
+        controller_input = qobj.to_dict()
+        for key, val in run_config.items():
+            if hasattr(val, 'to_dict'):
+                controller_input['config'][key] = val.to_dict()
+            else:
+                controller_input['config'][key] = val
+        # Execute on controller
+        return unitary_controller_execute(controller_input)
 
     def _validate(self, qobj, options):
         """Semantic validations of the qobj which cannot be done via schemas.
@@ -138,20 +180,20 @@ class UnitarySimulator(AerBackend):
         if n_qubits > max_qubits:
             raise AerError(
                 'Number of qubits ({}) is greater than max ({}) for "{}" with {} GB system memory.'
-                .format(n_qubits, max_qubits, name, int(local_hardware_info()['memory'])))
+                .format(n_qubits, max_qubits, name,
+                        int(local_hardware_info()['memory'])))
         if qobj.config.shots != 1:
-            logger.info('"%s" only supports 1 shot. Setting shots=1.',
-                        name)
+            logger.info('"%s" only supports 1 shot. Setting shots=1.', name)
             qobj.config.shots = 1
         for experiment in qobj.experiments:
             exp_name = experiment.header.name
             if getattr(experiment.config, 'shots', 1) != 1:
-                logger.info('"%s" only supports 1 shot. '
-                            'Setting shots=1 for circuit "%s".',
-                            name, exp_name)
+                logger.info(
+                    '"%s" only supports 1 shot. '
+                    'Setting shots=1 for circuit "%s".', name, exp_name)
                 experiment.config.shots = 1
             for operation in experiment.instructions:
                 if operation.name in ['measure', 'reset']:
                     raise AerError(
-                        'Unsupported {} instruction {} in circuit {}'
-                        .format(name, operation.name, exp_name))
+                        'Unsupported {} instruction {} in circuit {}'.format(
+                            name, operation.name, exp_name))
